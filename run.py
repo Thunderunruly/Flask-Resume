ea22c3bb03993c9474a966bff6bfc82d07f1907e
Thunderunruly <hyao2302@uni.sydney.edu.au>
2024-12-28 14:24:37 +1100

    update basic format of resume

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import json
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './static/files'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_filename_list(folder_path):
     try:
@@ -85,11 +96,41 @@ def edit_page():
         if form_id == 'language_form':
             lang = request.form.get('language', language)
             json_write(file_path,lang,"language")
-            return redirect(url_for("edit_page"))
-        if form_id == 'style_form':
+        elif form_id == 'style_form':
             css = request.form.get('style', style)
             json_write(file_path,css,"style")
-            return redirect(url_for("edit_page"))
+        elif form_id == "edit_form":
+            action = request.form.get('action')
+            if action == "save":
+                info = {}
+                for key, value in request.form.items():
+                    if key.startswith("info["):
+                        sub_key = key[5:-1]
+                        info[sub_key] = value
+                photo = request.files['photo']
+                if photo and photo.filename != '':
+                    if allowed_file(photo.filename):
+                        filename = secure_filename(photo.filename)
+                        saved_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                        photo.save(saved_file_path)
+                info['photo'] = "files/" + filename
+                json_write(file_path,info,"info")
+                # TODO MORE
+            elif action == "cancel":
+                pass
+            elif action == "add_info":
+                id = request.form['info-key']
+                info = content['info']
+                info[id] = "Default"
+                json_write(file_path, info, "info")
+            elif action == "delete_info":
+                id_to_delete = request.form['delete-info']
+                info = content['info']
+                if id_to_delete in info:
+                    del info[id_to_delete]
+                json_write(file_path, info, "info")
+        return redirect(url_for("edit_page"))
+
 
     return render_template("edit.html",
         data=content,
